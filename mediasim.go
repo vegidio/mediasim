711c48bfdf23824b3ad7dc09137f788347b25071
Vinicius Egidio <me@vinicius.io>
2025-06-08 14:11:37 +0200

    refactor: Change CompareMedia to return a channel for improved concurrency

--- mediasim.go
+++ mediasim.go
@@ -11,68 +11,72 @@ const Version = "<version>"
 // CompareMedia compares media files based on a similarity threshold. It returns a list of comparisons where each
 // comparison contains media files that are similar to each other.
 //
-// Parameters:
+// # Parameters:
 //   - media: an array of Media files to be compared.
 //   - threshold: a float64 value representing the similarity threshold.
 //
-// Returns:
-//   - An array of Comparison containing media files that are similar to each other.
-func CompareMedia(media []Media, threshold float64) []Comparison {
-	comparisons := make([]Comparison, 0)
-	compared := make([]bool, len(media))
+// # Returns:
+//   - <-chan Comparison: a channel that provides Comparison objects containing information about similar media files.
+func CompareMedia(media []Media, threshold float64) <-chan Comparison {
+	out := make(chan Comparison)
 
-	for i := 0; i < len(media); i++ {
-		if compared[i] {
-			continue
-		}
+	go func() {
+		defer close(out)
+		compared := make([]bool, len(media))
 
-		similarities := make([]Similarity, 0)
-
-		for j := i + 1; j < len(media); j++ {
-			if compared[j] {
+		for i := 0; i < len(media); i++ {
+			if compared[i] {
 				continue
 			}
 
-			var similarity float64
+			similarities := make([]Similarity, 0)
 
-			if media[i].Type == "image" && media[j].Type == "image" {
-				similarity = calculateImageSimilarity(media[i].Frames[0], media[j].Frames)
-			} else if media[i].Type == "video" && media[j].Type == "video" {
-				similarity = calculateVideoSimilarity(media[i].Frames, media[j].Frames)
-			} else {
-				continue
+			for j := i + 1; j < len(media); j++ {
+				if compared[j] {
+					continue
+				}
+
+				var similarity float64
+
+				if media[i].Type == "image" && media[j].Type == "image" {
+					similarity = calculateImageSimilarity(media[i].Frames[0], media[j].Frames)
+				} else if media[i].Type == "video" && media[j].Type == "video" {
+					similarity = calculateVideoSimilarity(media[i].Frames, media[j].Frames)
+				} else {
+					continue
+				}
+
+				if similarity >= threshold {
+					similarities = append(similarities, Similarity{
+						Name:  media[j].Name,
+						Score: similarity,
+					})
+
+					compared[i] = true
+					compared[j] = true
+				}
 			}
 
-			if similarity >= threshold {
-				similarities = append(similarities, Similarity{
-					Name:  media[j].Name,
-					Score: similarity,
+			if len(similarities) > 0 {
+				// Sort the similarities in descending order
+				slices.SortFunc(similarities, func(a, b Similarity) int {
+					if a.Score > b.Score {
+						return -1
+					} else if a.Score < b.Score {
+						return 1
+					}
+					return 0
 				})
 
-				compared[i] = true
-				compared[j] = true
+				out <- Comparison{
+					Name:         media[i].Name,
+					Similarities: similarities,
+				}
 			}
 		}
+	}()
 
-		if len(similarities) > 0 {
-			// Sort the similarities in descending order
-			slices.SortFunc(similarities, func(a, b Similarity) int {
-				if a.Score > b.Score {
-					return -1
-				} else if a.Score < b.Score {
-					return 1
-				}
-				return 0
-			})
-
-			comparisons = append(comparisons, Comparison{
-				Name:         media[i].Name,
-				Similarities: similarities,
-			})
-		}
-	}
-
-	return comparisons
+	return out
 }
 
 // region - Private functions
